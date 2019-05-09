339d969f9a28d395cf993c20a4c71d12c5574cd3
Victor Adeyanju <adeyanju.bincom@gmail.com>
2019-05-09 09:23:54 +0100

    Changed  files path

--- loan_prediction.py
+++ loan_prediction.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 %matplotlib inline
 
-test = pd.read_csv("test_Y3wMUE5_7gLdaTN.csv")
-train = pd.read_csv("")
+test = pd.read_csv("./dataset/test_Y3wMUE5_7gLdaTN.csv")
+train = pd.read_csv("t/dataset/train_u6lujuX_CVtuZ9i.csv")
 
 columns = df.columns
 head = df.head(20)
